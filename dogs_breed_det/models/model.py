ebf9df1aeeb37ce7aca811a085ef3049f704e149
root <valentin.kozlov@kit.edu>
2019-04-11 22:50:49 +0000

    remove Train_Args from get_metadata

--- dogs_breed_det/models/model.py
+++ dogs_breed_det/models/model.py
@@ -71,7 +71,6 @@ def get_metadata():
         'Author': None,
         'Author-email': None,
         'License': None,
-        'Train_args': cfg.train_args,
     }
 
     for line in pkg.get_metadata_lines("PKG-INFO"):
