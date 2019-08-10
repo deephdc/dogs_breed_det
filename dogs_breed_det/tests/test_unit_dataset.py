f707fa0b1194281fac5b4dbc0e0e67f5c961a1cc
valentin.kozlov <valentin.kozlov@kit.edu>
2019-08-10 12:22:59 +0200

    rename test_dataset to test_unit_dataset

---
+++ dogs_breed_det/tests/test_unit_dataset.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 - 2019 Karlsruhe Institute of Technology - Steinbuch Centre for Computing
+# This code is distributed under the MIT License
+# Please, see the LICENSE file
+#
+"""
+Created on Fri Aug  9 20:52:28 2019
+
+@author: vykozlov
+"""
+import os
+import numpy as np
+import dogs_breed_det.config as cfg
+import dogs_breed_det.dataset.data_utils as dutils
+
+def test_url_download():
+    url_path = cfg.Dog_RemoteShare
+    data_dir = os.path.join(cfg.BASE_DIR, 'dogs_breed_det/tests/tmp')
+    data_file = 'weights.best.ResNet50.hdf5'
+    
+    dist_exist, error_out = dutils.url_download(url_path, data_dir, data_file)
+    if dist_exist:
+        os.remove(os.path.join(data_dir, data_file))
+    
+    assert dist_exist
+
+def test_load_data_files():
+    path = os.path.join(cfg.BASE_DIR,'dogs_breed_det/tests/inputs/train')
+    file_list_ref = np.array([os.path.join(path, '001.Affenpinscher/Affenpinscher_00001.jpg'),
+                     os.path.join(path, '001.Affenpinscher/Affenpinscher_00002.jpg'),
+                     os.path.join(path, '016.Beagle/Beagle_01129.jpg'),
+                     os.path.join(path, '016.Beagle/Beagle_01131.jpg')])
+    file_list = dutils.load_data_files(path)
+    print("Found: ", np.sort(file_list))
+    print("Ref: ", file_list_ref)
+    
+    assert (file_list_ref == np.sort(file_list)).all()
+    
+def test_dog_names_create():
+    train_path = os.path.join(cfg.BASE_DIR,'dogs_breed_det/tests/inputs/train', '*')
+    labels_path = os.path.join(cfg.BASE_DIR,'dogs_breed_det/tests/tmp/tmp_dog_names.txt')
+    dog_names_ref = ['Affenpinscher', 'Beagle']
+    dog_names = dutils.dog_names_create(train_path, labels_path)
+    if os.path.exists(labels_path):
+        os.remove(labels_path)
+
+    print("Found: ", dog_names)
+    print("Ref: ", dog_names_ref)
+    
+    assert dog_names == dog_names_ref
+    
+def test_dog_names_load():
+    labels_path = os.path.join(cfg.BASE_DIR,'dogs_breed_det/tests/inputs/dog_names.txt')
+    dog_names_ref = ['Affenpinscher', 'Beagle', 'Collie', 'Mastiff', 'Saint_bernard']
+    dog_names = dutils.dog_names_load(labels_path)
+    print("Found: ", dog_names)
+    print("Ref: ", dog_names_ref)
+    
+    assert dog_names == dog_names_ref    
+
